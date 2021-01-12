5ba1bb945a8ec354af89515165118b1af291b6a8
vividfax <vividfax@gmail.com>
2021-01-12 16:15:50 +0000

    Render pearls

--- sketch.py
+++ sketch.py
@@ -10,7 +10,8 @@ def setup():
     bpy.context.scene.render.resolution_y = 1080
     bpy.context.scene.render.engine = 'CYCLES'
 
-    bpy.ops.object.light_add(type='SUN', location=(0, 0, 5))
+#    bpy.ops.object.light_add(type='SUN', location=(0, 0, 0))
+#    bpy.context.object.data.energy = 1000
     bpy.ops.object.camera_add(location=(0, 0, 25), rotation=(0, 0, 0))
     bpy.data.worlds["World"].node_tree.nodes["Background"].inputs[0].default_value = (
         1, 1, 1, 1)
@@ -22,31 +23,26 @@ def draw():
 
 
 def drawBackground():
+    mat = newMat("Plane")
+    mat.node_tree.nodes["Emission"].inputs[0].default_value = (
+        1, 0.570698, 0, 1)
+    mat.node_tree.nodes["Emission"].inputs[1].default_value = 1
+
     bpy.ops.mesh.primitive_plane_add(
         size=30, enter_editmode=False, align='WORLD', location=(0, 0, 0))
-
-    mat = bpy.data.materials.get("Plane")
-    if mat is None:
-        mat = bpy.data.materials.new(name="Plane")
-
-    bpy.data.materials["Plane"].node_tree.nodes["Emission"].inputs[0].default_value = (
-        1, 0.570698, 0, 1)
-    bpy.data.materials["Plane"].node_tree.nodes["Glossy BSDF"].inputs[1].default_value = 0.1
 
     bpy.context.active_object.data.materials.append(mat)
 
 
 def drawSpheres():
-    mat = bpy.data.materials.get("Material")
-    if mat is None:
-        mat = bpy.data.materials.new(name="Material")
-
-    bpy.data.materials["Material"].node_tree.nodes["Glossy BSDF"].inputs[0].default_value = (
+    mat = newMat("Material")
+    mat.node_tree.nodes["Glossy BSDF"].inputs[0].default_value = (
         0, 0.0394903, 0.212479, 1)
-    bpy.data.materials["Material"].node_tree.nodes["Glossy BSDF"].inputs[1].default_value = 0.1
+    mat.node_tree.nodes["Glossy BSDF"].inputs[1].default_value = 0.1
 
     for x in range(-4, 5):
         for y in range(-4, 5):
+
             z = randrange(0, 10)
             obj = bpy.ops.mesh.primitive_uv_sphere_add(
                 radius=.8, location=(x, y, z))
@@ -55,6 +51,13 @@ def drawSpheres():
             bpy.context.object.modifiers["Subdivision"].render_levels = 3
 
             bpy.context.active_object.data.materials.append(mat)
+
+
+def newMat(str):
+    mat = bpy.data.materials.get(str)
+    if mat is None:
+        mat = bpy.data.materials.new(name=str)
+    return mat
 
 
 setup()
