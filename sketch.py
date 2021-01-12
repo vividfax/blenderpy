9008c52701eba4550656b839242b1b608d2262c0
vividfax <vividfax@gmail.com>
2021-01-12 17:32:02 +0000

    Render gum

--- sketch.py
+++ sketch.py
@@ -9,12 +9,12 @@ def setup():
     bpy.context.scene.render.resolution_x = 1080
     bpy.context.scene.render.resolution_y = 1080
     bpy.context.scene.render.engine = 'CYCLES'
+    bpy.context.scene.cycles.samples = 64
 
-#    bpy.ops.object.light_add(type='SUN', location=(0, 0, 0))
-#    bpy.context.object.data.energy = 1000
-    bpy.ops.object.camera_add(location=(0, 0, 25), rotation=(0, 0, 0))
-    bpy.data.worlds["World"].node_tree.nodes["Background"].inputs[0].default_value = (
-        1, 1, 1, 1)
+    bpy.ops.object.light_add(type='SUN', location=(0, 0, 10))
+    bpy.context.object.data.energy = 10
+    bpy.ops.object.camera_add(location=(0, 0, 5), rotation=(0, 0, 0))
+    bpy.data.worlds["World"].node_tree.nodes["Background"].inputs[0].default_value = (1,1,1, 1)
 
 
 def draw():
@@ -24,28 +24,24 @@ def draw():
 
 def drawBackground():
     mat = newMat("Plane")
-    mat.node_tree.nodes["Emission"].inputs[0].default_value = (
-        1, 0.570698, 0, 1)
+    mat.node_tree.nodes["Emission"].inputs[0].default_value = (0.0231534, 0.031896, 0.0703601, 1)
     mat.node_tree.nodes["Emission"].inputs[1].default_value = 1
 
-    bpy.ops.mesh.primitive_plane_add(
-        size=30, enter_editmode=False, align='WORLD', location=(0, 0, 0))
+    bpy.ops.mesh.primitive_plane_add(size=300, enter_editmode=False, align='WORLD', location=(0, 0, 0))
 
     bpy.context.active_object.data.materials.append(mat)
 
 
 def drawSpheres():
     mat = newMat("Material")
-    mat.node_tree.nodes["Glossy BSDF"].inputs[0].default_value = (
-        0, 0.0394903, 0.212479, 1)
-    mat.node_tree.nodes["Glossy BSDF"].inputs[1].default_value = 0.1
+    bpy.data.materials["Material"].node_tree.nodes["Velvet BSDF"].inputs[0].default_value = (1, 0.185308, 0.233881, 1)
+    bpy.data.materials["Material"].node_tree.nodes["Diffuse BSDF"].inputs[0].default_value = (1, 0.185308, 0.233881, 1)
 
-    for x in range(-4, 5):
+    for x in range (-4, 5):
         for y in range(-4, 5):
 
-            z = randrange(0, 10)
-            obj = bpy.ops.mesh.primitive_uv_sphere_add(
-                radius=.8, location=(x, y, z))
+            rad = randrange(-1, 5)
+            obj = bpy.ops.mesh.primitive_uv_sphere_add(radius=rad, location=(x, y, 0))
 
             bpy.ops.object.modifier_add(type='SUBSURF')
             bpy.context.object.modifiers["Subdivision"].render_levels = 3
